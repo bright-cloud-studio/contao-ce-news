f786a9572cab0b2b51d3747d089ac3f8c1a4654c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-06 09:53:16 -0400

    Create BcsNewsBundle.php

---
+++ src/BcsNewsBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE News
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-news
+ */
+
+namespace Bcs\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsNewsBundle extends Bundle
+{
+}
