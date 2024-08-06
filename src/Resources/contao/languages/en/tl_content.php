70aa8f12f010ea00a8f1b34faf71a4e42b0fe92d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-06 14:41:36 -0400

    Update tl_content.php

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -1,8 +1,8 @@
 <?php
 
-  $GLOBALS['TL_LANG']['tl_content']['news_archive'] = array('Newsarchive', 'Please select the Newsarchive.');
+  $GLOBALS['TL_LANG']['tl_content']['news_archive'] = array('News Archive', 'Please select the Newsarchive.');
   $GLOBALS['TL_LANG']['tl_content']['news'] = array('News', 'Select a news.');
-  $GLOBALS['TL_LANG']['tl_content']['news_template'] = array('Newstemplate', 'Select a news template.');
+  $GLOBALS['TL_LANG']['tl_content']['news_template'] = array('News Template', 'Select a news template.');
   $GLOBALS['TL_LANG']['tl_content']['news_metaFields'] = array('Meta fields', 'Here you can select the meta fields.');
 
-  $GLOBALS['TL_LANG']['tl_content']['news_legend'] = 'News-Settings';
+  $GLOBALS['TL_LANG']['tl_content']['news_legend'] = 'News Settings';
