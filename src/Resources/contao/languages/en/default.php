cec6e8de3f14f4446cec754189922f4bc4883a59
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-06 14:36:06 -0400

    Create default.php

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,16 @@
+<?php
+
+  /**
+   * @copyright  Bright Cliud Studio
+   * @author     Bright Cloud Studio
+   * @package    Contao CE News
+   * @license    LGPL-3.0+
+   * @see	       https://github.com/bright-cloud-studio/contao-ce-news
+   */
+
+    $GLOBALS['TL_LANG']['CTE']['bcs'] = 'Bright Cloud Studio';
+    
+    $GLOBALS['TL_LANG']['CTE']['news_article'][0] = 'News Article';
+    $GLOBALS['TL_LANG']['CTE']['news_article'][1] = 'Select a News Article to display';
+
+?>
