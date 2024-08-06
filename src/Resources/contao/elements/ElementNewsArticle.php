5c0a1e93fe39d3a610a853af2bb2658e364ad1d2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-06 13:19:02 -0400

    Update ElementNewsArticle.php

--- src/Resources/contao/elements/ElementNewsArticle.php
+++ src/Resources/contao/elements/ElementNewsArticle.php
@@ -12,6 +12,7 @@ namespace Bcs\NewsArticleBundle;
 
 use Contao\BackendTemplate;
 use Contao\ContentText;
+use Contao\NewsModel;
 use Contao\System;
 
 class ContentNewsArticle extends ContentText
@@ -22,17 +23,33 @@ class ContentNewsArticle extends ContentText
 	/* Generate the content element */
 	public function compile()
 	{
+	    
+	    $objArticle = NewsModel::findPublishedByParentAndIdOrAlias($this->news, array($this->news_archive));
+	    
 		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
 		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
 		{
-			$this->strTemplate = 'be_wildcard';
-			$this->Template = new BackendTemplate($this->strTemplate);
-			$this->Template->title = $this->headline;
+		    
+			//$this->strTemplate = 'be_wildcard';
+			//$this->Template = new BackendTemplate($this->strTemplate);
+			//$this->Template->title = $this->headline;
+			
+			$objTemplate = new BackendTemplate('be_wildcard');
+			// requires existing template from autoload.ini
+			$this->news_template = 'news_latest';
+			$newsarticle = new ModuleNewsElement($objArticle, $this);
+			$objTemplate->wildcard = $newsarticle->generate();
+			
+			return $objTemplate->parse();
+			
+			
 		}
+		
+		$newsarticle = new ModuleNewsElement($objArticle, $this);
+		return $newsarticle->generate();
 
-		// Slider configuration
-		$this->Template->news_article = "SUCCESS";
+		
 	}
 
     
