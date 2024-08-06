8b686dd444dde583fa81fcc79c538432ad0b454d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-06 10:01:24 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE News
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-news
+ */
+
+$GLOBALS['TL_CTE']['bcs']['ce_news'] = 'Bcs\NewsBundle\ContentNewsArticle';
+
+?>
