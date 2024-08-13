f72cf58f15b8f780e30418b95549222032f2f78b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-13 13:13:04 -0400

    Update ElementNewsArticle.php

--- src/Resources/contao/elements/ElementNewsArticle.php
+++ src/Resources/contao/elements/ElementNewsArticle.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @copyright  Bright Cliud Studio
+ * @copyright  Bright Cloud Studio
  * @author     Bright Cloud Studio
  * @package    Contao CE News
  * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-news
  */
 
 namespace Bcs\NewsArticleBundle;
@@ -24,41 +24,32 @@ class ContentNewsArticle extends ContentText
 	/* Generate the content element */
 	public function generate()
 	{
-	    
+	    // Get the News Article selected in the Content Element
 	    $objArticle = NewsModel::findPublishedByParentAndIdOrAlias($this->news, array($this->news_archive));
-	    
-		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
+        // Find out where our Request is coming from
+		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
+        // If we have a request, and it is a Backend request, setup our backend template
 		if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
 		{
-		    
-			//$this->strTemplate = 'be_wildcard';
-			//$this->Template = new BackendTemplate($this->strTemplate);
-			//$this->Template->title = $this->headline;
-			
+            // Load the "backend wildcard" template
 			$objTemplate = new BackendTemplate('be_wildcard');
+            
 			// requires existing template from autoload.ini
-			$this->news_template = 'ce_news_article';
+			//$this->news_template = 'ce_news_article';
+
+            // Create a News Article module, apply our article to it
 			$newsarticle = new ModuleNewsArticle($objArticle, $this);
+
+            // Generate the News Article module then apply the HTML to our backend wildcard
 			$objTemplate->wildcard = $newsarticle->generate();
-			
-			//return $objTemplate->parse();
-			
-			
 		}
-		
+		// If this isnt a backend request, create a News Article module, apply our article
 		$newsarticle = new ModuleNewsArticle($objArticle, $this);
-		
-		//$this->Template->article = $newsarticle->generate();
+        // return the generated HTML
 		return $newsarticle->generate();
 
 		
 	}
-	
-	protected function compile()
-	{
-		return;
-	}
 
-    
 }
